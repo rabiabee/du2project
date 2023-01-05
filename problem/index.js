bb90f910e308a6ac4378a23c91320ecb8151000c
rabiabee <rabiaahm25@gmail.com>
2023-01-05 16:07:08 +0100

    script edit

--- problem/index.js
+++ problem/index.js
@@ -10,7 +10,7 @@
 
 */
 
-
+click_filter_element()
 
 // Create Filter Elements
 create_levels_filter();
--- problem/js/functions.js
+++ problem/js/functions.js
@@ -3,6 +3,10 @@
 // CODE According to specification
 function click_filter_element(event) {
 
+  const filter_element = event.target;
+  filter_element.classList.toggle("selected");
+  update_programmes();
+
   /*
     ARGUMENTS
       event: event-object created when user clicks on one of the filter elements.
